cfc4d66bcfad594e6459ebdc8f8f6890dc970d59
DBAN <dban7171@gmail.com>
2021-11-14 23:02:44 +0900

    Feat : API 호출 함수 추가
    현재 회원가입 기능은 테스트 코드로 구현은 해보았지만 아직까지 DB와 연동해서 작동을 해보지 않음
    API 호출 메소드를 모두 사용해서 API를 구현 할 예정
    DB스키마 구축 완료
    자세한 사항은 README에 사진으로 업로드 예정

--- rest_api.py
+++ rest_api.py
@@ -1,26 +1,40 @@
-from fastapi import *
+from fastapi import FastAPI,Depends
 from fastapi_login import LoginManager
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_login.exceptions import InvalidCredentialsException
 from starlette.responses import RedirectResponse
+from pydantic import BaseModel
 
 import uvicorn
 
 app = FastAPI()
 SECRET = '2c01b7facd47e87237b6160b40c255a2b51cdafd0aa98814'
 manager = LoginManager(SECRET, token_url='/auth/token')
-
 fake_db = {'johndoe@e.mail': {'password': 'hunter2'}}
 
+
+# ----------------------- Class  -----------------------
 
 class NotAuthenticatedException(Exception):
     pass
 
-@app.get("/")
-async def main():
-    main = "Account API"
-    return main
+class Item(BaseModel):
+    user_amount : str
+    user_memo: str
 
+
+
+
+
+# ----------------------- API Method  -----------------------
+
+
+# ----------------------- Register -----------------------
+# 회원가입 부분입니다.
+# ----------------------- Register -----------------------
+
+
+# ----------------------- Login -----------------------
 
 @app.post('/auth/token')
 def login(data: OAuth2PasswordRequestForm = Depends()):
@@ -41,11 +55,6 @@ def login(data: OAuth2PasswordRequestForm = Depends()):
 def exc_handler(request, exc):
     return RedirectResponse(url='/login')
 
-# This will be deprecated in the future
-# set your exception when initiating the instance
-# manager = LoginManager(..., custom_exception=NotAuthenticatedException)
-
-
 @manager.user_loader()
 def load_user(email: str):  # could also be an asynchronous function
     user = fake_db.get(email)
@@ -53,6 +62,38 @@ def load_user(email: str):  # could also be an asynchronous function
 manager.not_authenticated_exception = NotAuthenticatedException
 # You also have to add an exception handler to your app instance
 app.add_exception_handler(NotAuthenticatedException, exc_handler)
+
+# ----------------------- Login -----------------------
+
+
+# ----------------------- Request -----------------------
+
+@app.get("/")
+async def main():
+    main = "Account API"
+    return main
+
+@app.get("/accountmemo")
+async def get_account():
+
+    return
+
+@app.post("/accountmemo")
+async def post_account():
+
+    return
+
+@app.put("/accountmemo")
+async def put_account():
+
+    return
+
+@app.delete("/accountmemo")
+async def del_account():
+
+    return
+
+# ----------------------- Request -----------------------
 
 
 if __name__ == '__main__':
